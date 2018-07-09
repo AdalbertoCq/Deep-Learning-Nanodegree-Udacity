deb73bf2fc031103dd8444b12d86343d5b164bec
aclaudioquiros <aclaudioquiros@gmail.com>
2018-07-09 22:05:47 +0200

    Almost final

--- Reinforcement Learning/Quadcopter Project Actor-Critic RL/agents/agent.py
+++ Reinforcement Learning/Quadcopter Project Actor-Critic RL/agents/agent.py
@@ -6,27 +6,42 @@ from agents.OUNoise import OUNoise
 from agents.ExperienceReplay import ExperienceReplayBuffer
 
 class Actor:
-    def __init__(self, state_space, action_space, action_min, action_max, hidden_units, learning_rate):
+    def __init__(self, state_space, action_space, action_min, action_max, hidden_units, learning_rate, q_lambda):
         self.state_space = state_space
         self.action_space = action_space
         self.action_max = action_max
         self.action_min = action_min
         self.action_range = action_max - action_min
         self.learning_rate = learning_rate
+        self.q_lambda = q_lambda
         
         # Neural Network definition.
         
         # Batch normalization on the state input and all layers of U network.
-        var_wi = initializers.lecun_normal()
+        # var_wi = initializers.VarianceScaling(scale=1.0/3.0, mode='fan_in', distribution='uniform')
+        var_wi = initializers.VarianceScaling(scale=1.0, mode='fan_in', distribution='uniform')
+        out_wi = initializers.RandomUniform(minval=-3e-3, maxval=3e-3)
+        
+        # Input
         input_states = layers.Input(shape=(self.state_space,), name='input_states')
         
-        layer = layers.Dense(units=2*hidden_units, kernel_initializer=var_wi, activation='elu')(input_states)
-        layer = layers.Dense(units=hidden_units, kernel_initializer=var_wi, activation='elu')(layer)
+        # Layer 1
+        layer = layers.Dense(units=400, kernel_regularizer=regularizers.l2(self.q_lambda))(input_states)
+        layer = layers.BatchNormalization()(layer)
+        layer = layers.Activation('relu')(layer)
         
-        norm_action = layers.Dense(self.action_space, kernel_initializer=var_wi, activation='sigmoid', name='norm_action')(layer)
+        # Layer 2
+        layer = layers.Dense(units=300, kernel_regularizer=regularizers.l2(self.q_lambda))(layer)
+        layer = layers.BatchNormalization()(layer)       
+        layer = layers.Activation('relu')(layer)
+        
+        # Output between 0,1.
+        norm_action = layers.Dense(self.action_space, kernel_initializer=out_wi, activation='sigmoid',
+                                   name='norm_action')(layer)
         
         # Adapt actions for the range in which rotors work.
         actions = layers.Lambda(lambda x: (x*self.action_range) + self.action_min, name='actions')(norm_action)
+        ############## 
         
         # Instantiate Model.
         self.model = models.Model(input=input_states, output=actions)
@@ -47,35 +62,52 @@ class Actor:
         
         
 class Critic:
-    def __init__(self, state_space, action_space, hidden_units, learning_rate):
+    def __init__(self, state_space, action_space, hidden_units, learning_rate, q_lambda):
         self.state_space = state_space
         self.action_space = action_space
         self.hidden_units = hidden_units
         self.learning_rate = learning_rate
+        self.q_lambda = q_lambda
         
         # Network Architecture.
-        var_wi = initializers.lecun_normal()
+        # var_wi = initializers.VarianceScaling(scale=1.0/3.0, mode='fan_in', distribution='uniform')
+        var_wi = initializers.VarianceScaling(scale=1.0, mode='fan_in', distribution='uniform')
+        out_wi = initializers.RandomUniform(minval=-3e-3, maxval=3e-3)
     
-        # States network.
-        input_states = layers.Input(shape=(self.state_space,), name='input_states')
-        layer_states = layers.Dense(units=2*hidden_units, kernel_initializer=var_wi, activation='elu')(input_states)
-
-        # Actions network.
-        input_actions = layers.Input(shape=(self.action_space,), name='input_actions') 
-        layer_actions = layers.Dense(units=2*hidden_units, kernel_initializer=var_wi, activation='elu')(input_actions)
-
-        # Try different layer sizes, activations, add batch normalization, regularizers, etc.
-
-        # Advantage network.
-        layer = layers.Add()([layer_states, layer_actions])
-        layer = layers.Dense(units=hidden_units, kernel_initializer=var_wi, activation='elu')(layer)
         
+        ## States network.        
+        # Input States
+        input_states = layers.Input(shape=(self.state_space,), name='input_states')
 
-        # Add more layers to the combined network if needed
+        # Layer 1.
+        layer_states = layers.Dense(units=400, kernel_regularizer=regularizers.l2(self.q_lambda))(input_states)
+        layer_states = layers.BatchNormalization()(layer_states)
+        layer_states = layers.Activation('relu')(layer_states)
+        
+        # Layer 2.
+        layer_states = layers.Dense(units=300, activation='relu',
+                                    kernel_regularizer=regularizers.l2(self.q_lambda))(layer_states)
+        
+        
+        ## Action network.
+        # Input Actions
+        input_actions = layers.Input(shape=(self.action_space,), name='input_actions') 
+
+        # Layer 1.
+        layer_actions = layers.Dense(units=300, activation='relu',
+                                     kernel_regularizer=regularizers.l2(self.q_lambda))(input_actions)
+        
+        
+        ## Advantage network.
+        layer = layers.Add()([layer_states, layer_actions])
+        layer = layers.Activation('relu')(layer)
+        # layer = layers.Dense(units=hidden_units, kernel_initializer=var_wi, activation='relu',
+        #                     kernel_regularizer=regularizers.l2(self.q_lambda))(layer)
 
         # Add final output layer to prduce action values (Q values)
-        q_values = layers.Dense(units=1, kernel_initializer=var_wi, name='q_values')(layer)
-    
+        q_values = layers.Dense(units=1, kernel_initializer=out_wi, name='q_values')(layer)
+        
+        
         
         # Instantiate the model
         self.model = models.Model(inputs=[input_states, input_actions], outputs=q_values)
@@ -101,12 +133,14 @@ class DDPG_Agent:
         
         # Instantiate Actors and Critics.
         self.actor = Actor(self.state_space, self.action_space, self.action_low, self.action_high, hidden_units=nn_hidden[0],
-                           learning_rate=actor_lr)
+                           learning_rate=actor_lr, q_lambda=q_lambda)
         self.actor_target = Actor(self.state_space, self.action_space, self.action_low, self.action_high,
-                                  hidden_units=nn_hidden[0], learning_rate=actor_lr)
+                                  hidden_units=nn_hidden[0], learning_rate=actor_lr, q_lambda=q_lambda)
         
-        self.critic = Critic(self.state_space, self.action_space, hidden_units=nn_hidden[1], learning_rate=critic_lr)
-        self.critic_target = Critic(self.state_space, self.action_space, hidden_units=nn_hidden[1], learning_rate=critic_lr)
+        self.critic = Critic(self.state_space, self.action_space, hidden_units=nn_hidden[1], learning_rate=critic_lr, 
+                             q_lambda=q_lambda)
+        self.critic_target = Critic(self.state_space, self.action_space, hidden_units=nn_hidden[1], learning_rate=critic_lr, 
+                             q_lambda=q_lambda)
         
         # Set same weights in target.
         self.actor_target.model.set_weights(self.actor.model.get_weights())
@@ -194,6 +228,7 @@ class DDPG_Agent:
         
         # Getting gradients before Critics backprop.
         action_gradients = self.critic.get_action_gradients([states, actions, 0])
+        action_gradients_prev = action_gradients
         action_gradients = np.reshape(action_gradients[0], (-1, self.action_space))
         
         # Learning Phase = 0 (Test), we just want the gradient, no update on weights.
--- Reinforcement Learning/Quadcopter Project Actor-Critic RL/task.py
+++ Reinforcement Learning/Quadcopter Project Actor-Critic RL/task.py
@@ -3,8 +3,7 @@ from physics_sim import PhysicsSim
 
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
-    def __init__(self, init_pose=None, init_velocities=None, 
-        init_angle_velocities=None, runtime=5., target_pos=None):
+    def __init__(self, init_pose=None, init_velocities=None, init_angle_velocities=None, runtime=5., target_pos=None, action_repeat=3, debug=False):
         """Initialize a Task object.
         Params
         ======
@@ -16,34 +15,87 @@ class Task():
         """
         # Simulation
         self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
-        self.action_repeat = 3
+        self.action_repeat = int(action_repeat)
 
-        self.state_size = self.action_repeat * 6
-        self.action_low = 0
+
+        self.state_size = self.action_repeat * self.sim.pose.shape[0]
+        self.action_low = 1
         self.action_high = 900
         self.action_size = 4
+        self.runtime = runtime
+        self.debug = debug
 
         # Goal
         self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
-
-    def get_reward(self):
+    def get_reward(self, done):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
-        return reward
+        
+        # Reward for X,Y axis.
+        reward_position = 0.4*(1-0.01*abs(self.sim.pose[:2] - self.target_pos[:2]).sum())
+        # Reinforce more movement across z axis.
+        reward_z_axis = 1-0.01*abs(self.sim.pose[2] - self.target_pos[2])
+        
+        
+        ## Need to find a way to handle velocities and penalize the ones that move the target away.
+        # Take destination as reference, substract point from current position taking destiantion as axis origin reference.
+        # Given position and current acceleration, reward it.
+        # If negative, below objective, positive acceleration.
+        #reference_position_z = self.sim.pose[2] - self.target_pos[2]
+        #reward_accel_z = np.clip(a=-reference_position_z*self.sim.linear_accel[2]/5, a_min=-1, a_max=+1)
+        #reference_position_h = self.sim.pose[:2] - self.target_pos[:2]
+        #reward_accel_h = 0.3*np.clip(a=-reference_position_h*self.sim.linear_accel[:2]/5, a_min=-1, a_max=+1).sum()
+        
+        # Reward speed on z axis.
+        reward_speed_z = 0.2*np.clip(a=self.sim.v[2]/5, a_min=-1, a_max=+1)
+        #reward_accel_z = np.clip(a=self.sim.linear_accel[2]/5, a_min=-1, a_max=+1)
+        
+        reward = (reward_position + reward_z_axis)/1.4
+        
+        # Reward getting to the requested hight.
+        if self.sim.pose[2] > self.target_pos[2]:
+            reward += 5
+            done = True
+        # Penalize crash.
+        #elif done and self.sim.time < self.runtime:
+        #    reward += -10
+        #    done = True
+        # Penalize moving but not getting to the requested hight.
+        #elif self.sim.time > self.runtime:
+        #    reward += -5
+        #    done = True
+        
+        if self.debug:
+            print('------ Step --------')
+            print('Position', self.sim.pose[:3], 'Z Speed', self.sim.v[2])
+            print()
+            print('Position reward', reward_position)
+            print('Position Z reward', reward_z_axis)
+            #print('Speed Z reward', reward_speed_z)
+            print('Final Reward', reward)
+        return reward, done
 
+    
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
         reward = 0
         pose_all = []
-        for _ in range(self.action_repeat):
+        for i in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
+            
+            reward_step, done = self.get_reward(done) 
+            reward += reward_step
             pose_all.append(self.sim.pose)
+            if done:
+                while len(pose_all)<self.action_repeat:
+                    pose_all.append(self.sim.pose)
+                break
+        
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
 
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
+        # self.sim.pose[:3] = [0., 0. , np.random.normal(0.5, 0.1)]
         state = np.concatenate([self.sim.pose] * self.action_repeat) 
         return state
